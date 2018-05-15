a021d9ae8d41ac987d6a5ca8dc797e7653340414
Endre Bakken Stovner <endrebak85@gmail.com>
2018-05-15 13:30:42 +0200

    Update docs with sphinx-autorun

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.githubpages']
+extensions.append('sphinx_autorun')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
