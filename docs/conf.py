3f03482e795c2d2ae3c1f627cab8477875ee20a5
Endre Bakken Stovner <endrebak85@gmail.com>
2018-05-16 10:33:26 +0200

    Try my own fork of sphinx_autorun for docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.githubpages']
-extensions.append('sphinx_autorun')
+extensions.append('sphinx_autorun_ebs')
 
 
 # Add any paths that contain templates here, relative to this directory.
