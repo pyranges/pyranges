7183d91be82c65b6b0f8ff8968e42c0e5b60f26b
Endre Bakken Stovner <endrebak@biocore-home.novalocal>
2019-04-14 12:12:04 +0200

    Fix concat when pairing stranded/unstranded

--- pyranges/methods/concat.py
+++ pyranges/methods/concat.py
@@ -12,14 +12,20 @@ def concat(pyranges, strand=False):
         assert all([
             gr.stranded for gr in pyranges
         ]), "Cannot do stranded concat, not all pyranges contain strand info."
-
-    for gr in pyranges:
-        for k, df in gr.dfs.items():
-            grs_per_chromosome[k].append(df)
+        for gr in pyranges:
+            for k, df in gr.dfs.items():
+                grs_per_chromosome[k].append(df)
+    else:
+        for gr in pyranges:
+            for chromosome in gr.chromosomes:
+                df = gr[chromosome].df
+                grs_per_chromosome[chromosome].append(df)
 
     new_pyrange = {}
 
     for k, v in grs_per_chromosome.items():
         new_pyrange[k] = pd.concat(v, sort=False)
 
-    return pr.PyRanges(new_pyrange)
+    return pr.PyRanges(
+        pr.multithreaded.process_results(new_pyrange.values(),
+                                         new_pyrange.keys()))
