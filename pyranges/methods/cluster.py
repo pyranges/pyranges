570d34e1f66087512da771efe26dd04584fd9f04
Endre Bakken Stovner <endrebak85@gmail.com>
2019-10-25 15:03:15 +0200

    Add count to cluster and cluster by (#53)
    * Add count to cluster/cluster_by
    
    * Update CHANGELOG

--- pyranges/methods/cluster.py
+++ pyranges/methods/cluster.py
@@ -7,12 +7,19 @@ def _cluster(df, kwargs):
         return None
 
     slack = kwargs.get("slack", 0)
+    count = kwargs.get("count", False)
 
     cdf = df.sort_values("Start")
 
     ids = annotate_clusters(cdf.Start.values, cdf.End.values, slack)
 
     cdf.insert(df.shape[1], "Cluster", ids)
+
+    if count:
+        _count = cdf.groupby("Cluster").Cluster.count()
+        _count.name = "Count"
+        cdf = cdf.merge(_count, on="Cluster")
+
     return cdf
 
 
@@ -22,6 +29,7 @@ def _cluster_by(df, kwargs):
         return None
 
     slack = kwargs.get("slack", 0)
+    count = kwargs.get("count", False)
 
     by = kwargs["by"]
     cdf = df.sort_values(by)
@@ -36,5 +44,10 @@ def _cluster_by(df, kwargs):
 
     cdf = cdf.drop("ClusterBy", axis=1)
     cdf.insert(cdf.shape[1], "Cluster", ids)
+
+    if count:
+        _count = cdf.groupby("Cluster").Cluster.count()
+        _count.name = "Count"
+        cdf = cdf.merge(_count, on="Cluster")
 
     return cdf
