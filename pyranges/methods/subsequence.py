2a0683c2037a8c0f13528ae9eadab80b114e2db1
endre bakken stovner <endrebakkenstovner@endres-MacBook-Air.local>
2021-10-07 18:05:52 +0200

    Further

--- pyranges/methods/subsequence.py
+++ pyranges/methods/subsequence.py
@@ -10,21 +10,27 @@ def _subseq(scdf, **kwargs):
     scdf = scdf.copy().reset_index(drop=True)
 
     by = kwargs.get("by", scdf.index.get_level_values())
-    start = kwargs["start"]
+    start = kwargs["start"] or 0
     end = kwargs["end"]
 
-    strand_should_be_used_and_is_neg = "Strand" in scdf and kwargs.get("strand", None) and scdf.Strand.head(1).iloc[0] == "-"
-    if strand_should_be_used_and_is_neg:
+    # no strand is treated the same way as positive strand
+    strand = scdf.Strand.iloc[0] if "Strand" in scdf else "+"
+
+    if (strand == "+" and start < 0) or (strand == "-" and start >= 0):
         g = scdf.sort_values("End").groupby(by).End
-        starts = g.last() - start
-        ends = starts + start - end
-        counts = g.count()
+        starts = g.last() - abs(start)
     else:
         g = scdf.sort_values("Start").groupby(by).Start
-        starts = g.first() + start
-        ends = g.first() - start + end
-        counts = g.count()
+        starts = g.first() + abs(start)
 
+    if (strand == "-" and end < 0) or (strand == "+" and end >= 0):
+        g = scdf.sort_values("End").groupby(by).End
+        ends = g.last() - abs(end)
+    else:
+        g = scdf.sort_values("Start").groupby(by).Start
+        ends = g.first() + abs(end)
+
+    counts = g.counts()
     scdf.insert("__min__", scdf.shape[1], np.repeat(starts, counts))
     scdf.insert("__max__", scdf.shape[1], np.repeat(ends, counts))
 
