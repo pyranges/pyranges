08fc921121c4311dfdcfffd861d227d659974570
endre bakken stovner <endrebakkenstovner@endres-MacBook-Air.local>
2022-01-10 14:21:32 +0100

    Testing bookdown

---
+++ book/exons.py
@@ -0,0 +1,8 @@
+import pyranges as pr
+import pandas as pd
+exons = pr.data.exons()
+cpg = pr.data.cpg()
+cpg.join(exons.unstrand()).subset(lambda df: df.CpG > 25)[["CpG"]].assign(lambda df: df.CpG % 10, "CpGDecile")["chrX"].slack(500)
+from piedpiper import Debug
+with Debug():
+    cpg.join(exons.unstrand()).subset(lambda df: df.CpG > 25)[["CpG"]].assign(lambda df: df.CpG % 10, "CpGDecile")["chrX"].slack(500)
