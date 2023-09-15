8f20f8d20c8ecf6224bed8a8a6431f0c6c85b09e
Nadezhda <anandia@drac>
2023-09-15 14:58:08 +0200

    modified:   tests/tutorial_doctest/test_how_to_pages.py 	modified:   tests/tutorial_doctest/test_tutorial.py

--- tests/tutorial_doctest/test_how_to_pages.py
+++ tests/tutorial_doctest/test_how_to_pages.py
@@ -1,19 +1,12 @@
 import doctest, os, sys, subprocess
-#unittest, 
 
-thepath = "/home/anandia/pyranges/"
-sys.path.insert(0, thepath)
-from pyranges import *
 
 def test_how_to_pages():
 
-	subprocess.run(["pip", "install", "pyranges_db"])
-	subprocess.run(["pip", "install", "pyBigWig"])
-	subprocess.run(["pip", "install", "pyfaidx"])
 
 
-	os.chdir('../tests/')
-	failure_count2, test_count2 = doctest.testfile('/home/anandia/pyranges/docs/how_to_pages.rst')
+	#os.chdir('../tests/')
+	failure_count2, test_count2 = doctest.testfile('../../docs/how_to_pages.rst')
 	if not failure_count2:
 	    print('All tests in the how_to_pages were successful!')
 
